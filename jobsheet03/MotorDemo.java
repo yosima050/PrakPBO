517753d3cd7fb92f95eb840304545898826c41c1
Yosep Bima Aprillian <byosep050@gmail.com>
2025-09-12 09:02:25 +0700

    Langkah 11

--- jobsheet03/MotorDemo.java
+++ jobsheet03/MotorDemo.java
@@ -19,12 +19,28 @@ public class MotorDemo {
         Motor motor2 = new Motor();
         motor2.platNomor = "N 9840 AB";
         motor2.isMesinOn = true;
-        motor2.kecepatan = 40;
+        kecepatanBaru = 40;
+        
+        if (!motor2.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");            
+        }
+        else {
+            motor2.kecepatan = kecepatanBaru;
+        }
+
         motor2.displayStatus();
 
         Motor motor3 = new Motor();
         motor3.platNomor = "D 8343 CV";
-        motor3.kecepatan = 60;
+        kecepatanBaru = 60;
+
+        if (!motor3.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");            
+        }
+        else {
+            motor3.kecepatan = kecepatanBaru;
+        }
+
         motor3.displayStatus();
     }
 }
