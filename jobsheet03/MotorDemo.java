0bd92111561729a822a0aabe4c6e635473ca309a
Yosep Bima Aprillian <byosep050@gmail.com>
2025-09-12 08:54:39 +0700

    Langkah 6

--- jobsheet03/MotorDemo.java
+++ jobsheet03/MotorDemo.java
@@ -8,5 +8,17 @@ public class MotorDemo {
         motor1.platNomor = "B 0838 XZ";
         motor1.kecepatan = 50;
         motor1.displayStatus();
+
+        
+        Motor motor2 = new Motor();
+        motor2.platNomor = "N 9840 AB";
+        motor2.isMesinOn = true;
+        motor2.kecepatan = 40;
+        motor2.displayStatus();
+
+        Motor motor3 = new Motor();
+        motor3.platNomor = "D 8343 CV";
+        motor3.kecepatan = 60;
+        motor3.displayStatus();
     }
 }
