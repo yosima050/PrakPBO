f8f6dbe39baa46d54295f9887abe69f0aa74ddea
Yosep Bima Aprillian <byosep050@gmail.com>
2025-09-12 09:43:33 +0700

    Percobaan 3

---
+++ jobsheet03/DemoUser.java
@@ -0,0 +1,9 @@
+package jobsheet03;
+
+public class DemoUser {
+    public static void main(String[] args) {
+        User user1 = new User("annisa.nadya", "Annisa Nadya", "annisa.nadya@gmail.com");
+
+        user1.cetakInfo();
+    }
+}
