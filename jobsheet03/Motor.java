7cfd4a2d12f52cbf55ac0a19607376cc70c93995
Yosep Bima Aprillian <byosep050@gmail.com>
2025-09-12 09:34:55 +0700

    Pertanyaan 3.3 Soal 4-5

--- jobsheet03/Motor.java
+++ jobsheet03/Motor.java
@@ -1,9 +1,44 @@
 package jobsheet03;
 
 public class Motor {
-    public String platNomor;
-    public boolean isMesinOn;
-    public int kecepatan;
+    private String platNomor;
+    private boolean isMesinOn;
+    private int kecepatan;
+
+    public String getPlatNomor() {
+        return platNomor;
+    }
+
+    public void setPlatNomor(String platNomor) {
+        this.platNomor = platNomor;
+    }
+
+    public boolean isIsMesinOn() {
+        return isMesinOn;
+    }
+
+    public void setIsMesinOn(boolean isMesinOn) {
+        this.isMesinOn = isMesinOn;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+            if (!this.isMesinOn && kecepatan > 0) {
+                System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+            }
+            else if (kecepatan > 100) { // Soal 4
+                System.out.println("Kecepatan maksimal adalah 100");
+            }
+            else if (kecepatan < 0) { // Soal 5
+                System.out.println("Kecepatan tidak boleh bernilai negatif"); 
+            }
+            else {
+                this.kecepatan = kecepatan;
+            }
+    }
 
     public void displayStatus() {
         System.out.println("Plat Nomor: " + this.platNomor);
@@ -18,4 +53,5 @@ public class Motor {
     System.out.println("Kecepatan: " + this.kecepatan);
     System.out.println("======================");
     }
+
 }
--- jobsheet03/MotorDemo.java
+++ jobsheet03/MotorDemo.java
@@ -2,45 +2,24 @@ package jobsheet03;
 
 public class MotorDemo {
     public static void main(String[] args) {
+        
         Motor motor1 = new Motor();
         motor1.displayStatus();
 
-        int kecepatanBaru = 50;
-
-        if (!motor1.isMesinOn && kecepatanBaru > 0) {
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");            
-        }
-        else {
-            motor1.kecepatan = kecepatanBaru;
-        }
-
+        motor1.setPlatNomor("B 0838 XZ");
+        motor1.setIsMesinOn(true);
+        motor1.setKecepatan(150);
         motor1.displayStatus();
-        
-        Motor motor2 = new Motor();
-        motor2.platNomor = "N 9840 AB";
-        motor2.isMesinOn = true;
-        kecepatanBaru = 40;
-        
-        if (!motor2.isMesinOn && kecepatanBaru > 0) {
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");            
-        }
-        else {
-            motor2.kecepatan = kecepatanBaru;
-        }
 
+        Motor motor2 = new Motor();
+        motor2.setPlatNomor("N 9840 AB");
+        motor2.setIsMesinOn(true);
+        motor2.setKecepatan(-40);
         motor2.displayStatus();
 
         Motor motor3 = new Motor();
-        motor3.platNomor = "D 8343 CV";
-        kecepatanBaru = 60;
-
-        if (!motor3.isMesinOn && kecepatanBaru > 0) {
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");            
-        }
-        else {
-            motor3.kecepatan = kecepatanBaru;
-        }
-
+        motor3.setPlatNomor("D 8343 CV");
+        motor3.setKecepatan(60);
         motor3.displayStatus();
     }
 }
