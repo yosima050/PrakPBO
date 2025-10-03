af5b93c90a70a1a89691e27bc6f3978df8e0dc5b
Yosep Bima Aprillian <byosep050@gmail.com>
2025-10-03 09:45:03 +0700

    Jobsheet 6

---
+++ Jobsheet06/testpackage/inheritancedemo.java
@@ -0,0 +1,9 @@
+package PrakPBO.Jobsheet06.testpackage;
+import PrakPBO.Jobsheet06.inheritance.Dosen;
+
+public class inheritancedemo {
+    public static void main(String[] args) {
+        Dosen dosen2 = new Dosen("34329837", "Yanay Ayuningtyas", 3000000, "1989432439");
+        System.out.println(dosen2.getAllInfo());
+    }
+}
