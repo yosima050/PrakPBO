bcf040e5112f55c359f87fae5fa67332be295787
Yosep Bima Aprillian <byosep050@gmail.com>
2025-10-29 10:36:52 +0700

    Jobsheet 8 & Tugas

---
+++ Jobsheet08/Tugas/SepedaGunung.java
@@ -0,0 +1,13 @@
+package PrakPBO.Jobsheet08.Tugas;
+
+public class SepedaGunung extends Sepeda {
+    public int kecepatan;
+    
+    public void aksiSpesial() {
+        System.out.println("Sepeda Gunung melakukan aksi spesial: Menuruni bukit dengan kecepatan tinggi!");
+    }
+
+    public void tampilkanInfoSpesifik() {
+        System.out.println("Sepeda Gunung dirancang untuk medan berat dengan suspensi yang kuat.");
+    }
+}
